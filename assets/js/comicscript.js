bc1ddfa9ed6c1ef7c2c6bcf0b3c638ac8ca70607
Melissa Bentley <melivesel@gmail.com>
2024-05-13 18:47:24 -0500

    add apis back in to get to work

--- assets/js/comicscript.js
+++ assets/js/comicscript.js
@@ -1,54 +1,42 @@
-const appendComicUrlToDiv = function(character) {
-    fetch('./vendorapis.json')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            const apiPublicKey = data.marvel.publicKey;
-            const apiPrivateKey = data.marvel.privateKey;
-            const apiUrlComic = `https://gateway.marvel.com:443/v1/public/characters?name=${character}&apikey=${apiPublicKey}`;
-            fetch(apiUrlComic)
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(data) {
-                    const characterId = data.data.results[0].id;
-                    fetch(`https://gateway.marvel.com:443/v1/public/characters/${characterId}/comics?limit=5&apikey=${apiPublicKey}`)
-                        .then(function(response) {
-                            return response.json();
-                        })
-                        .then(function(data) {
-                            const comics = data.data.results;
-                            const searchResultDiv = document.getElementById('comicsuggestion');
-                            searchResultDiv.innerHTML = '';
-                            searchResultDiv.style.fontFamily = 'IBM Plex Sans, sans-serif';
-                            comics.forEach(function(comic) {
-                                const comicTitle = comic.title;
-                                const comicTitleElement = document.createElement('h2');
-                                const comicDescription = comic.description;
-                                const comicDescriptionElement = document.createElement('p');
-                                comicTitleElement.textContent = comicTitle;
-                                searchResultDiv.appendChild(comicTitleElement);
+fetch("https://gateway.marvel.com:443/v1/public/characters?name=Iron%20Man&apikey=f99ff0ebd9b29727ddc4d22f632170a4")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        const characterId = data.data.results[0].id;
 
-                                if (comicDescription) {
-                                    comicDescriptionElement.textContent = comicDescription;
-                                } else {
-                                    comicDescriptionElement.textContent = "No description available";
-                                }
+        fetch(`https://gateway.marvel.com:443/v1/public/characters/${characterId}/comics?limit=5&apikey=f99ff0ebd9b29727ddc4d22f632170a4`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                const comics = data.data.results;
+                const searchResultDiv = document.getElementById('comicsuggestion');
+                searchResultDiv.innerHTML = ''; 
+                searchResultDiv.style.fontFamily = 'IBM Plex Sans, sans-serif';
 
-                                searchResultDiv.appendChild(comicDescriptionElement);
-                            });
-                        })
-                        .catch(function(error) {
-                            console.log("Error fetching data:", error);
-                        });
-                })
-                .catch(function(error) {
-                    console.log("Error fetching data:", error);
+                comics.forEach(function(comic) {
+                    const comicTitle = comic.title;
+                    const comicTitleElement = document.createElement('h2');
+                    const comicDescription = comic.description;
+                    const comicDescriptionElement = document.createElement('p');
+                    comicTitleElement.textContent = comicTitle;
+                    searchResultDiv.appendChild(comicTitleElement);
+
+                    if (comicDescription) {
+                        comicDescriptionElement.textContent = comicDescription;
+                    } else {
+                        comicDescriptionElement.textContent = "No description available";
+                    }
+
+                    searchResultDiv.appendChild(comicDescriptionElement);
+
                 });
-        })
-        .catch(function(error) {
-            console.log("Error fetching data:", error);
-        });
-}
-appendComicUrlToDiv("iron man")
+            })
+            .catch(function(error) {
+                console.log("Error fetching data:", error);
+            });
+    })
+    .catch(function(error) {
+        console.log("Error fetching data:", error);
+    });
